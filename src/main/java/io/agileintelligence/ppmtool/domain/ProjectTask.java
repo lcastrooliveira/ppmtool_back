1e4631aa8b8ecdcb2d1358114933214ade7e0497
Lucas Oliveira <lcastro.oliveira@gmail.com>
2020-05-12 23:00:30 -0300

    Backlog and Project Entities

---
+++ src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
@@ -0,0 +1,58 @@
+package io.agileintelligence.ppmtool.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+public class ProjectTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false)
+    private String projectSequence;
+
+    @NotBlank(message = "Please include a project summary")
+    private String summary;
+    private String acceptanceCriteria;
+    private String status;
+    private Integer priority;
+    private Date dueDate;
+    // ManyToOne with Backlog
+    @Column(updatable = false)
+    private String projectIdentifier;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public ProjectTask() {}
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+
+    @Override
+    public String toString() {
+        return "ProjectTask{" +
+                "id=" + id +
+                ", projectSequence='" + projectSequence + '\'' +
+                ", summary='" + summary + '\'' +
+                ", acceptanceCriteria='" + acceptanceCriteria + '\'' +
+                ", status='" + status + '\'' +
+                ", priority=" + priority +
+                ", dueDate=" + dueDate +
+                ", projectIdentifier='" + projectIdentifier + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
