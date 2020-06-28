d6784f74ecaa2796d7cab2653016d2e994969dd2
Lucas Oliveira <lcastro.oliveira@gmail.com>
2020-06-28 16:00:42 -0300

    User registration part 3

--- src/main/java/io/agileintelligence/ppmtool/web/UserController.java
+++ src/main/java/io/agileintelligence/ppmtool/web/UserController.java
@@ -3,6 +3,7 @@ package io.agileintelligence.ppmtool.web;
 import io.agileintelligence.ppmtool.domain.User;
 import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.UserService;
+import io.agileintelligence.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,22 +20,26 @@ import javax.validation.Valid;
 public class UserController {
 
     private final MapValidationErrorService mapValidationErrorService;
+    private final UserValidator userValidator;
     private final UserService userService;
 
     @Autowired
-    public UserController(MapValidationErrorService mapValidationErrorService, UserService userService) {
+    public UserController(MapValidationErrorService mapValidationErrorService,
+                          UserValidator userValidator,
+                          UserService userService) {
         this.mapValidationErrorService = mapValidationErrorService;
+        this.userValidator = userValidator;
         this.userService = userService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
-        // validate passwords match
+        userValidator.validate(user, result);
         ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
         if(errorMap != null) return errorMap;
-        
         final User newUser = userService.saveUser(user);
-        
-        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+        newUser.setPassword("");
+        newUser.setConfirmPassword("");
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 }
