1b318c42a9c51ebc46727d1eccbb1b0696f3094c
Lucas Oliveira <lcastro.oliveira@gmail.com>
2020-07-31 22:55:02 -0300

    Add react static files

---
+++ src/main/resources/static/precache-manifest.1355009241a84802bbe0efab3ec47234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfe5f0140ba12c23bfac575714fce4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2808277fc47d4b76e6bd",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "227d3f649b57d03c86de",
+    "url": "/static/css/main.7be3ab8b.chunk.css"
+  },
+  {
+    "revision": "2808277fc47d4b76e6bd",
+    "url": "/static/js/2.ef207d35.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ef207d35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "227d3f649b57d03c86de",
+    "url": "/static/js/main.39f203bd.chunk.js"
+  },
+  {
+    "revision": "e1a819c583cbbd5a5de3",
+    "url": "/static/js/runtime-main.8bb83d80.js"
+  }
+]);
